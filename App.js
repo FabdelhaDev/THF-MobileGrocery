e61fc095fb65c575a46a028abd17ff104fb49894
Hani Abdelmajid <130587964+HaniAbdelmajid@users.noreply.github.com>
2025-11-20 14:37:08 -0600

    Changes

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TabsNavigator from './navigation/TabsNavigator';
@@ -5,19 +6,28 @@ import SignInScreen from './screens/SignInScreen';
 import FoldersScreen from './screens/FoldersScreen';
 import ListScreen from './screens/ListScreen';
 import ItemScreen from './screens/ItemScreen';
+import RecipeDetailScreen from './screens/RecipeDetailScreen'; // ensure this exists
+import { ItemsProvider } from './context/ItemsContext';
+import { RecipesProvider } from './context/RecipesContext';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-    return (
+  return (
+    <ItemsProvider>
+      <RecipesProvider>
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="SignIn" component={SignInScreen} />
-                <Stack.Screen name="Tabs" component={TabsNavigator} />
-                <Stack.Screen name="FolderScreen" component={FoldersScreen} />
-                <Stack.Screen name="ListScreen" component={ListScreen} />
-                <Stack.Screen name="ItemScreen" component={ItemScreen} />
-            </Stack.Navigator>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="Tabs" component={TabsNavigator} />
+            <Stack.Screen name="FolderScreen" component={FoldersScreen} />
+            <Stack.Screen name="ListScreen" component={ListScreen} />
+            <Stack.Screen name="ItemScreen" component={ItemScreen} />
+            {/* Do NOT put plain text here, comments must be inside braces like this */}
+            <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
+          </Stack.Navigator>
         </NavigationContainer>
-    );
+      </RecipesProvider>
+    </ItemsProvider>
+  );
 }
