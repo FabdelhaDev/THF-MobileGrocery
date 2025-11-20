0c3a1ab0d6d2d9d14599796df3699a747c34d897
Hani Abdelmajid <130587964+HaniAbdelmajid@users.noreply.github.com>
2025-11-20 14:30:24 -0600

    Changes - Hani

--- navigation/TabsNavigator.js
+++ navigation/TabsNavigator.js
@@ -1,3 +1,5 @@
+// Helps us move between tabs essentially
+import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import SettingsScreen from '../screens/SettingsScreen';
 import FoldersTab from '../tabs/FoldersTab';
@@ -7,13 +9,13 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const Tab = createMaterialTopTabNavigator();
 
 export default function TabsNavigator() {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Tab.Navigator>
-                <Tab.Screen name="Folders" component={FoldersTab} />
-                <Tab.Screen name="Recipes" component={RecipesTab} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tab.Navigator>
+        <Tab.Screen name="Folders" component={FoldersTab} />
+        <Tab.Screen name="Recipes" component={RecipesTab} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </SafeAreaView>
+  );
 }
